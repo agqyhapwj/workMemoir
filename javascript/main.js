bc49bb68bf1fa035386b68085c61e1e3f22f2ca7
agqyhapwj <agqyhapwj555@gmail.com>
2022-07-09 22:03:16 +0800

    2022 0709修改成function，要用時呼叫即可

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,11 @@
-var year = new Date().getFullYear(); //宣告變數 year 來承接 new Date()
-document.getElementById('Year').textContent = year;
+// console.log()是控制台列印語法
+// 最新年份日期函數 Date 產生每年都會自動更新的年份
+// 宣告變數 year 來承接 new Date()
+// 寫成函式要用時呼叫 "function Name()"即可自動執行，例：NewYear();
 
-// console.log(year) //此行可在控制台印出執行結果
+NewYear();
+function NewYear() {
+    var year = new Date().getFullYear();
+    document.getElementById('Year').textContent = year;
+    // console.log(year);
+}
