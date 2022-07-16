30c8a42a907cc971e90e23cb656ed588bbefa78a
agqyhapwj <agqyhapwj555@gmail.com>
2022-07-16 22:04:14 +0800

    2022  0716建立header.js的function 隱藏、開啟、關閉選單

---
+++ web/javascript/header.js
@@ -0,0 +1,22 @@
+// MenuNavOpenShow();
+function MenuNavOpenShow() {
+    let Menu = document.getElementById('MenuNavOpen');
+    Menu.style.display = 'none';
+}
+
+MenuNavOpenShow();
+function MenuNavOpenShow() {
+    // $ jQuery的變數前綴詞
+    // 隱藏選單
+    $('#MenuNavOpen').hide();
+
+    // 開啟選單
+    $('MenuBtn').click( function() {
+        $('MenuNavOpen').show();
+    })
+
+    // 關閉選單
+    $('#Close').click( function() {
+        $('MenuNavOpen').hide();
+    })
+}
